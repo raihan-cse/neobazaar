164221befeaad7465e8a125e66876c3044b49e5c
Raihan Kabir <raihancse46@gmail.com>
2021-01-10 20:50:07 +0600

    Frist commit > all projects

---
+++ assets/js/menubar.js
@@ -0,0 +1,19 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "280px";
+    document.getElementById("dark-bg").style.display = 'block';
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("dark-bg").style.display = 'none';
+  }
+  function openNav2() {
+    document.getElementById("mySidenav-2").style.width = "280px";
+    document.getElementById("dark-bg").style.display = 'block';
+  }
+  
+  function closeNav2() {
+    document.getElementById("mySidenav-2").style.width = "0";
+    document.getElementById("dark-bg").style.display = 'none';
+  }
